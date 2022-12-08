e4c9ca97edf84ae679a8c9f5bfdba6cad7b173dc
cloudemprise <cloudemprise@users.noreply.github.com>
2022-12-08 14:00:59 +0000

    A Simple Factory design pattern.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # go-dsgn-pttrn-simple-factory
-TBD
+
+> A Go implementation of the Simple Factory design pattern.
+
+Because Go does not support traditional OOP components, such as classes and inheritance, it is not possible to implement the classic Factory Method. Nevertheless, it is still possible to implement a basic Simple Factory pattern within Go.
+
+The Simple Factory Pattern is a creational design pattern that creates objects by calling a free-standing factory function that constructs a particular variant of the specified type. As such, concrete object creation is implemented by derived types rather than by calling a constructor of the primal type itself, thus providing a mechanism to mutate or extend variations of the interested type.
+
+&nbsp;
+
+<p align="center">
+  <img src="./go-dsgn-pttrn-simple-factory.svg">
+</p>
---
+++ go-dsgn-pttrn-simple-factory.go
@@ -0,0 +1,129 @@
+package main
+
+/* simple factory pattern */
+
+import "fmt"
+
+// v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
+// START : Abstract Product Definition (Interface)
+
+// require product behaviours
+type iAbstractProduct interface {
+	setBehaviour(behaviour string)
+	getBehaviour() string
+	setFeature(feature int)
+	getFeature() int
+}
+
+// END : Abstract Product Definition (Interface)
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
+// START : Concrete Product Definition
+
+// the concrete product type
+type concreteProduct struct {
+	behaviour string
+	feature   int
+}
+
+// the concrete product implementation:
+// ---------------------------------
+
+func (item *concreteProduct) setBehaviour(behaviour string) {
+	item.behaviour = behaviour
+}
+
+func (item *concreteProduct) getBehaviour() string {
+	return item.behaviour
+}
+
+func (item *concreteProduct) setFeature(feature int) {
+	item.feature = feature
+}
+
+func (item *concreteProduct) getFeature() int {
+	return item.feature
+}
+
+// END : Concrete Product Definition
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
+// START : Custom Concrete Product Types
+
+// (abstract) custom product definition
+type customProductA struct {
+	concreteProduct
+}
+
+// free-standing constructor function where variation is declared
+func newCustomProductA() iAbstractProduct {
+	var item = new(concreteProduct)
+	// mutate the basal type
+	item.setBehaviour("Custom Product A")
+	item.setFeature(1)
+	return item
+}
+
+// ---
+
+// (abstract) custom product definition
+type customProductB struct {
+	concreteProduct
+}
+
+// free-standing constructor function where variation is declared
+func newCustomProductB() iAbstractProduct {
+	var item = new(concreteProduct)
+	// mutate the basal type
+	item.setBehaviour("Custom Product B")
+	item.setFeature(2)
+	return item
+}
+
+// END : Custom Concrete Product Types
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
+// START : A Simple Factory Function
+
+// free-standing product selector
+func createProduct(productType string) (iAbstractProduct, error) {
+	if productType == "customProductA" {
+		return newCustomProductA(), nil
+	}
+	if productType == "customProductB" {
+		return newCustomProductB(), nil
+	}
+	return nil, fmt.Errorf("Wrong product selection.")
+}
+
+// START : A Simple Factory Function
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
+// START : Client Code
+
+func main() {
+
+	myProductA, _ := createProduct("customProductA")
+	printDetails(myProductA)
+
+	myProductB, _ := createProduct("customProductB")
+	printDetails(myProductB)
+}
+
+// Test Function
+func printDetails(product iAbstractProduct) {
+	fmt.Printf("Behaviour: %s\n", product.getBehaviour())
+	fmt.Printf("Feature  : %d\n", product.getFeature())
+}
+
+// END : Client Code
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
