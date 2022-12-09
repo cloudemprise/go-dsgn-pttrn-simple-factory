968f6101508350fab3090f697e6a9bfb431f0b15
cloudemprise <cloudemprise@users.noreply.github.com>
2022-12-09 18:48:40 +0000

    Included different ways to make a factory function

--- go-dsgn-pttrn-simple-factory.go
+++ go-dsgn-pttrn-simple-factory.go
@@ -2,7 +2,9 @@ package main
 
 /* simple factory pattern */
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
 // START : Abstract Product Definition (Interface)
@@ -52,20 +54,21 @@ func (item *concreteProduct) getFeature() int {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
-// START : Custom Concrete Product Types
+// START : Custom Concrete Product Factory Functions
 
 // (abstract) custom product definition
 type customProductA struct {
 	concreteProduct
 }
 
-// free-standing constructor function where variation is declared
+// factory function : one way of doing it
 func newCustomProductA() iAbstractProduct {
-	var item = new(concreteProduct)
-	// mutate the basal type
-	item.setBehaviour("Custom Product A")
-	item.setFeature(1)
-	return item
+	return &customProductA{
+		concreteProduct{
+			behaviour: "Special default",
+			feature:   0,
+		},
+	}
 }
 
 // ---
@@ -75,34 +78,54 @@ type customProductB struct {
 	concreteProduct
 }
 
-// free-standing constructor function where variation is declared
+// factory function : a different way of doing it
+func newCustomProductB() iAbstractProduct {
+	// var item = &customProductB{}
+	var item = new(customProductB)
+	item.behaviour = "Different default value"
+	item.feature = 0
+	return item
+}
+
+/* A 3rd way of doing it
+// free-standing factory function where variation is declared
 func newCustomProductB() iAbstractProduct {
 	var item = new(concreteProduct)
 	// mutate the basal type
 	item.setBehaviour("Custom Product B")
 	item.setFeature(2)
 	return item
-}
+} */
 
-// END : Custom Concrete Product Types
+// END : Custom Concrete Product Factory Functions
 // ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
-// START : A Simple Factory Function
+// START : Product Selector
 
-// free-standing product selector
-func createProduct(productType string) (iAbstractProduct, error) {
-	if productType == "customProductA" {
-		return newCustomProductA(), nil
+// a product selector with some mutation
+func createProduct(productType string) iAbstractProduct {
+	switch productType {
+	case "customProductA":
+		product := newCustomProductA()
+		// mutate the default values
+		product.setBehaviour("Custom Product A")
+		product.setFeature(123)
+		return product
+	case "customProductB":
+		product := newCustomProductB()
+		// mutate the default values
+		product.setBehaviour("Custom Product B")
+		product.setFeature(456)
+		return product
+	default:
+		return nil
 	}
-	if productType == "customProductB" {
-		return newCustomProductB(), nil
-	}
-	return nil, fmt.Errorf("Wrong product selection.")
+
 }
 
-// END : A Simple Factory Function
+// END : Product Selector
 // ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -111,10 +134,12 @@ func createProduct(productType string) (iAbstractProduct, error) {
 
 func main() {
 
-	myProductA, _ := createProduct("customProductA")
+	myProductA := createProduct("customProductA")
 	printDetails(myProductA)
 
-	myProductB, _ := createProduct("customProductB")
+	myProductB := createProduct("customProductB")
+	myProductB.setBehaviour("Need unique change here.")
+	myProductB.setFeature(69)
 	printDetails(myProductB)
 }
 
